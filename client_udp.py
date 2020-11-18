d9fbd774a16df6d52e4068fa796007df3bba12b8
Vitor <vmm3@cin.ufpe.br>
2020-11-18 16:00:45 -0300

    Update

--- client_udp.py
+++ client_udp.py
@@ -5,43 +5,63 @@ import json
 
 class ClientUDP:
 
-	def __init__(self, name_input, server_ip, server_port):
-		self.sock = socket(AF_INET, SOCK_DGRAM)
-		self.dest = (server_ip, server_port)
-		self.name = name_input
-		Thread(target=self.recv_data, args=()).start()
+    def __init__(self, name_input, server_ip, server_port):
+        self.sock = socket(AF_INET, SOCK_DGRAM)
+        self.dest = (server_ip, server_port)
+        self.name = name_input
+        Thread(target=self.recv_data, args=()).start()
 
-	def get_name(self):
-		return self.name
+    def get_name(self):
+        return self.name
 
-	def send_data(self, data):
-		self.sock.sendto(data.encode(), self.dest)
+    def send_data(self, data):
+        self.sock.sendto(data.encode(), self.dest)
 
-	def recv_data(self):
-		while True:
-			data, server = self.sock.recvfrom(1500)
-			if not data:
-				break
-			else:
-				print('Mensagem recebida: {}'.format(data.decode('utf-8')))
+    def recv_data(self):
+        while True:
+            data, server = self.sock.recvfrom(1500)
+            if not data:
+                break
+            else:
+                print('Mensagem do servidor {}: {}'.format(server, data.decode('utf-8')))
 
 
 def get_ip():
-	sock = socket(AF_INET, SOCK_DGRAM)
-	local_ip = sock.connect(('8.8.8.8', 80))
-	local_ip = sock.getsockname()[0]
-	return local_ip
+    sock = socket(AF_INET, SOCK_DGRAM)
+    local_ip = sock.connect(('8.8.8.8', 80))
+    local_ip = sock.getsockname()[0]
+    return local_ip
 
 
 def main():
-	print('Bem vindo ao Quiz Competitivo!\nPara participar informe alguns dados...')
-	client_name = input('Digite seu nome: ')
-	ip, port = input('Endereço IP do servidor: '), int(input('Porta do servidor: '))
-	return client_name, ip, port
+    print('Bem vindo(a) ao Quiz competitivo!')
+    client_name = input("Digite seu nome: ")
+    ip = input('Endereço do servidor: ')
+    port = int(input('Porta do servidor: '))
+    return client_name, ip, port
+
+
+def menu():
+    print('\nSelecione o número da opção desejada: \n')
+    opt = input('1 - Iniciar partida\n2 - Lista de jogadores\n3 - Ajuda\n>> ')
+    if opt == '1':
+        client_udp.send_data(opt)
+    elif opt == '2':
+        client_udp.send_data(opt)
+        return menu()
+    elif opt == '3':
+        print('Ao iniciar a partida cada jogador iniciará com 100 pontos\n'
+              'Resposta correta: + 25 pontos\n'
+              'Resposta incorreta: -5 pontos\n'
+              'Sem resposta: -1 ponto\n'
+              'As respostas são compostas por uma única palavra, com caracteres minúsculos\n'
+              'Além disso cada jogador terá 10 segundos para responder.')
+        return menu()
 
 
 if __name__ == '__main__':
-	name, ip, port = main()
-	client_udp = ClientUDP(name, ip, port)
-	info = dict(name=name, ip=get_ip())
-	client_udp.send_data(json.dumps(info))
+    name, ip, port = main()
+    client_udp = ClientUDP(name, ip, port)
+    info = [name, get_ip()]
+    client_udp.send_data(json.dumps(info))
+    menu()
--- server_udp.py
+++ server_udp.py
@@ -7,61 +7,64 @@ import os
 
 class ServerUDP:
 
-	def __init__(self, host, port):
-		self.server = socket(AF_INET, SOCK_DGRAM)
-		self.server.bind((host, port))
-		self.clients_list, self.num_of_clients = [], 0
-		self.chosen_numbers = []
-		Thread(target=self.recv_data, args=()).start()
+    def __init__(self, host, port):
+        self.server = socket(AF_INET, SOCK_DGRAM)
+        self.server.bind((host, port))
+        self.clients_list, self.num_of_clients = [], 0
+        self.chosen_numbers = []
+        Thread(target=self.recv_data, args=()).start()
 
-	def get_clients_list(self):
-		return self.clients_list
+    def get_clients_list(self):
+        return self.clients_list
 
-	def player_manager(self, info):
-		if self.num_of_clients < 5:
-			self.clients_list.append(json.loads(info))
-			self.num_of_clients += 1
-			print(self.clients_list)
-		else:
-			print('Limite máximo de participantes atingido!')
+    def player_manager(self, info, address=None):
+        if self.num_of_clients < 5:
+            self.clients_list.append(json.loads(info))
+            self.num_of_clients += 1
+        else:
+            self.send_data('1 - Limite máximo de jogadores atigindo! Deseja iniciar a partida?', address)
 
-	@staticmethod
-	def file_reader(index):
-		path = os.getcwd()
-		with open(path + '/tuplas.txt', 'r', encoding='utf-8') as file:
-			question = file.readlines()
-		print(question[index])
-		return question[index]
+    @staticmethod
+    def file_reader(index):
+        path = os.getcwd()
+        with open(path + '/tuplas.txt', 'r', encoding='utf-8') as file:
+            question = file.readlines()
+        return question[index]
 
-	def question_manager(self, client_address):
-		n = random.randint(0, 20)
-		while True:
-			if n in self.chosen_numbers:
-				n = random.randint(0, 20)
-			else:
-				if len(self.chosen_numbers) == 20:
-					print('Todas as perguntas foram enviadas aos clientes')
-					break
-				else:
-					self.chosen_numbers.append(n)
-					self.send_data(self.file_reader(n), client_address)
-					print(f'Pergunta enviada ao jogador: {self.file_reader(n)}')
-					break
+    def question_manager(self, client_address):
+        n = random.randint(0, 20)
+        while True:
+            if n in self.chosen_numbers:
+                n = random.randint(0, 20)
+            else:
+                if len(self.chosen_numbers) == 20:
+                    print('Todas as perguntas foram enviadas aos clientes')
+                    break
+                else:
+                    self.chosen_numbers.append(n)
+                    self.send_data(self.file_reader(n), client_address)
+                    print(f'Pergunta enviada ao jogador: {self.file_reader(n)}')
+                    break
 
-	def recv_data(self):
-		while True:
-			print('Aguardando requisições...')
-			data, client = self.server.recvfrom(1500)
-			if not data:
-				break
-			else:
-				print('Cliente {} enviou: {}'.format(client, data.decode('utf-8')))
-				msg = input("Envie algo para o cliente: ")
-				self.send_data(msg, client)
+    def recv_data(self):
+        while True:
+            print('Aguardando requisições...')
+            data, client = self.server.recvfrom(1500)
+            if not data:
+                break
+            else:
+                if isinstance(json.loads(data.decode()), list) and self.num_of_clients <= 5:
+                    print("O cliente {} enviou: {}".format(client, data.decode()))
+                    self.player_manager(data.decode(), client)
+                else:
+                    if data.decode() == '1':
+                        print('Start()')
+                    elif data.decode() == '2':
+                        self.send_data(json.dumps(self.get_clients_list()), client)
 
-	def send_data(self, data, address):
-		self.server.sendto(data.encode(), address)
+    def send_data(self, data, address):
+        self.server.sendto(data.encode(), address)
 
 
 if __name__ == '__main__':
-	server_udp = ServerUDP('localhost', 8080)
+    server_udp = ServerUDP('localhost', 8080)
